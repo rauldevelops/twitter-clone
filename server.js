915acaf3390fc8289cd17f48b9a32f47bfcdbfeb
Raul Develops <124627746+rauldevelops@users.noreply.github.com>
2026-01-01 04:01:41 -0500

    installed express, nodemon, cors, and set up a basic server along with express.static for static files

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+const PORT = 8000
+
+app.use(cors())
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
